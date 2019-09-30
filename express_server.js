3be1beda9d89948ddd06e311289082053ecba11b
Najeam <nmehanma@gmal.com>
2019-09-30 18:09:10 +0000

    created express server

---
+++ express_server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8080; // default port 8080
+
+
+const urlDatabase = {
+  "b2xVn2": "http://ligthouselabs.com",
+  "9sm5xK": "http://www.google.com"
+
+};
+
+app.get("/", (req, res)=> {
+  res.send("Hello!");
+});
+
+app.listen(PORT,()=> {
+  console.log(`Example app listening  on port ${PORT}!`)
+})
