41a6cdc549566ee991f8f0a6b06e8cd517102cbd
Najeam <nmehanma@gmal.com>
2019-09-30 21:45:49 +0000

    adding a Post route to receive the Form submission

--- express_server.js
+++ express_server.js
@@ -3,11 +3,18 @@ const app = express();
 
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 //set the view engine to ejs
+
+
 
 app.set("view engine", "ejs");
 
 //use res.render to load up an ejs view file
+
+//our server responds by getting the "urls_new" template to display to the client generating the HTML
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -42,6 +49,12 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello<b>World</b></body></html")
 } )
+
+app.post("/urls",(req,res) => {
+  console.log(req.body);   //Log the POST request body to the console
+  res.send("OK");          //Respond with 'OK' (we will replace this)
+});
+
 
 app.listen(PORT,()=> {
   console.log(`Example app listening  on port ${PORT}!`)
